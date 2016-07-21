150297343107e5ab6ad39fc3daa966efcffa4e50
DHINAKAR-P <dhinakarintech@gmail.com>
2016-07-21 18:13:14 +0530

    ionic and cordova camera widget

---
+++ ionic_starter/www/app/js/controllers/AplicationLevelCtrl.js
@@ -0,0 +1,4 @@
+app.controller("AplicationLevelCtrl", [ '$scope', '$rootScope', '$location', '$window', '$q', '$http', '$cordovaSQLite',
+				    function( $scope, $rootScope, $location, $window, $q, $http, $cordovaSQLite) {
+  $location.path('/app/landing_page-en');
+}]);
